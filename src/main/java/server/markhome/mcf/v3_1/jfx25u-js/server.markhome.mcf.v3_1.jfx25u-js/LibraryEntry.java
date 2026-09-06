bc82ea01fe581d929ecdaad28b9a6b4f25c2a931
Mark Sobkow <mark.sobkow@gmail.com>
2026-09-06 09:12:02 -0600

    2026-09-06 09:15 CST Work in progress; I have so many packages to migrate it is necessary to automate key aspects of the GraalVM integration process, such as creating a default main for the packages.

---
+++ src/main/java/server/markhome/mcf/v3_1/jfx25u-js/server.markhome.mcf.v3_1.jfx25u-js/LibraryEntry.java
@@ -0,0 +1,16 @@
+package server.markhome.mcf.v3_1.cflib;
+
+public class CFLibLibraryEntry {
+
+	public static int main(int argc, String[] argv) {
+		return 0;
+	}
+
+	public static String getCFLibName() {
+		return ("server.markhome.mcf.v3_1.cflib");
+	}
+
+	public static String getCFLibVersion() {
+		return ("3.1.42");
+	}
+}
